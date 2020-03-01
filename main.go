9511df2bd02de9be77039c8b2eeac15f445df343
tomiokashougo <tomiokasyogo@gmail.com>
2020-03-02 01:14:15 +0900

    remove app.go

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"os"
+
+	"github.com/urfave/cli/v2"
 )
 
 const version = "0.0.1"
@@ -14,4 +17,40 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func newApp() *cli.App {
+	app := cli.NewApp()
+	app.Name = "grouper"
+	app.Usage = "Force grouped import path"
+	app.Version = fmt.Sprintf("%s-%s", version, revision)
+	app.Authors = []*cli.Author{{
+		Name:  "tommy-sho",
+		Email: "tomiokasyogo@gmail.com",
+	}}
+	app.Action = grouper
+	app.Flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:    "local",
+			Aliases: []string{"l"},
+			Usage:   "specify imports prefix beginning with this string after 3rd-party packages. especially your own organization name. comma-separated list",
+		},
+		&cli.BoolFlag{
+			Name:    "write",
+			Aliases: []string{"w"},
+			Usage:   "write result source to original file instead od stdout",
+		},
+	}
+
+	return app
+}
+
+func grouper(c *cli.Context) error {
+	env := Env{
+		Paths:       c.Args().Slice(),
+		Write:       c.Bool("write"),
+		LocalPrefix: c.String("local"),
+	}
+
+	return grouperMain(env)
 }
